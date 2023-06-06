743606ef3bffc1daedce3f039d97f22879d44317
Ibrahem Chahine <22155702+ibrahimchahine@users.noreply.github.com>
2023-06-06 21:59:31 +0300

    debug

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,13 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using Fusion;
 using UnityEngine;
 
-public class ScoreManager : MonoBehaviour
+public class ScoreManager : NetworkBehaviour
 {
     [SerializeField] NumberField ScoreDisplay;
 
+    [Networked(OnChanged = nameof(ScoreChanged))]
+    public int Score { get; set; } = 0;
+
+    private static void ScoreChanged(Changed<ScoreManager> changed)
+    {
+        // Here you would add code to update the player's healthbar.
+        Debug.Log($"Health changed to: {changed.Behaviour.Score}");
+        changed.Behaviour.ScoreDisplay.SetNumber(changed.Behaviour.Score);
+    }
     public void AddScore(int score)
     {
-        ScoreDisplay.SetNumber(ScoreDisplay.GetNumber() + score);
+        Score += score;
     }
 }
