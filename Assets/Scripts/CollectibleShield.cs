21c5bed0a7eef44175025ba9a16080993249d983
Ibrahem Chahine <22155702+ibrahimchahine@users.noreply.github.com>
2023-06-06 16:38:17 +0300

    upadte

--- Assets/Scripts/CollectibleShield.cs
+++ Assets/Scripts/CollectibleShield.cs
@@ -5,17 +5,19 @@ using UnityEngine;
 public class CollectibleShield : MonoBehaviour
 {
     public float shieldDuration = 3f; // Duration of the shield in seconds
-
+    public bool sheildOn = false;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            ActivateShield(shieldDuration);
+            // ActivateShield(shieldDuration);
+            Invoke("ActivateShield", shieldDuration);
             Destroy(gameObject);
         }
     }
     private void ActivateShield(float shieldDuration)
     {
-
+        sheildOn = !sheildOn;
     }
+
 }
